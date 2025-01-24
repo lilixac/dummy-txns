419c848ab892a70133fb25c22bc6eebfce56272f
lilixac <hinewton25@gmail.com>
2025-01-24 15:33:46 +0545

    configurable param

--- script2.sh
+++ script2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in $(seq 1 5); do
     echo "Calling"
-cast send 0x5FbDB2315678afecb367f032d93F642f64180aa3 "setNumber()()" --rpc-url http://127.0.0.1:8545  --private-key 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d -- --broadcast
+cast send $1 "setNumber()()" --rpc-url http://127.0.0.1:8545  --private-key 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d -- --broadcast
 done
--- wrapper.sh
+++ wrapper.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Call each script in the background
-./script1.sh &
-./script2.sh &
-./script3.sh &
-./script4.sh &
+address=$(forge create --rpc-url http://127.0.0.1:8545 --private-key 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d src/Counter.sol:Counter --json | jq -r .deployedTo)
+
+echo $address
+
+# # Call each script in the background
+./script1.sh $address &
+./script2.sh $address &
+./script3.sh $address &
+./script4.sh $address &
 
 # Wait for all background jobs to finish
 wait
